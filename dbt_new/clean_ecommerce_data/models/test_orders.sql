355ebabec65c6d0d228e4580ad7cc4d7cdaf5b1f
duytruong1211 <duytruong1211@gmail.com>
2025-03-09 15:31:53 +0700

    add test orders

---
+++ dbt_new/clean_ecommerce_data/models/test_orders.sql
@@ -0,0 +1,11 @@
+
+
+{{ config(materialized='view') }}
+
+SELECT
+    order_id,
+    customer_id,
+    order_status,
+    order_purchase_timestamp::date AS order_date
+FROM {{ source('raw', 'olist_orders') }}
+WHERE order_status NOT IN ('canceled', 'unavailable')
