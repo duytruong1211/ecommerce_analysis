7d82e3c52c418f2f80f4b10e713c487091789297
duytruong1211 <duytruong1211@gmail.com>
2025-03-29 15:45:34 +0700

    switch location

--- ecommerce_analysis/ecommerce_dwh/models/staging/staging_users.sql
+++ ecommerce_analysis/ecommerce_dwh/models/staging/staging_users.sql
@@ -2,8 +2,8 @@
 with cte as ( select * from {{ source('raw', 'olist_order_customer_dataset') }} )
 
 SELECT
-    customer_id user_order_id,
     customer_unique_id user_id,
+    customer_id user_order_id,
     customer_zip_code_prefix first_five_zip_user,
     unaccent(customer_city) user_city,
     customer_state user_state
