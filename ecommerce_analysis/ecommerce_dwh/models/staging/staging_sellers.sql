fbf787b84cfa47c3754be9b87c44d8899c94efc8
duytruong1211 <duytruong1211@gmail.com>
2025-03-29 15:39:34 +0700

    strip special quotes from city columns

--- ecommerce_analysis/ecommerce_dwh/models/staging/staging_sellers.sql
+++ ecommerce_analysis/ecommerce_dwh/models/staging/staging_sellers.sql
@@ -2,11 +2,10 @@
 with cte as ( select * from {{ source('raw', 'olist_sellers_dataset') }} )
 
 SELECT
-    customer_id user_order_id,
-    customer_unique_id user_id,
-    customer_zip_code_prefix first_five_zip,
-    customer_city user_city,
-    customer_state user_state
+    seller_id,
+    customer_zip_code_prefix first_five_zip_seller,
+    unaccent(seller_city) seller_city,
+    seller_state
 FROM 
     cte
 
--- ecommerce_analysis/ecommerce_dwh/models/staging/staging_users.sql
+++ ecommerce_analysis/ecommerce_dwh/models/staging/staging_users.sql
@@ -5,7 +5,7 @@ SELECT
     customer_id user_order_id,
     customer_unique_id user_id,
     customer_zip_code_prefix first_five_zip_user,
-    customer_city user_city,
+    unaccent(customer_city) user_city,
     customer_state user_state
 FROM 
     cte
