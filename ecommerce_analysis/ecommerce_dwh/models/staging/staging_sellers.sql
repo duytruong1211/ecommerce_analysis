a0c255aaa5e9747134b3f09e845e30b581cf9d13
duytruong1211 <duytruong1211@gmail.com>
2025-03-29 15:34:53 +0700

    rename column

--- ecommerce_analysis/ecommerce_dwh/models/staging/staging_sellers.sql
+++ ecommerce_analysis/ecommerce_dwh/models/staging/staging_sellers.sql
@@ -1,10 +1,10 @@
 
-with cte as ( select * from {{ source('raw', 'olist_order_customer_dataset') }} )
+with cte as ( select * from {{ source('raw', 'olist_sellers_dataset') }} )
 
 SELECT
     customer_id user_order_id,
     customer_unique_id user_id,
-    customer_zip_code_prefix first_five_zip_customer,
+    customer_zip_code_prefix first_five_zip,
     customer_city user_city,
     customer_state user_state
 FROM 
