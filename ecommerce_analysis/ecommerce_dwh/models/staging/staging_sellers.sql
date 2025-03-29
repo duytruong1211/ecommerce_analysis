dc45f763a648c9fc26e2083dc045602235e81b7c
duytruong1211 <duytruong1211@gmail.com>
2025-03-29 15:44:05 +0700

    typo

--- ecommerce_analysis/ecommerce_dwh/models/staging/staging_sellers.sql
+++ ecommerce_analysis/ecommerce_dwh/models/staging/staging_sellers.sql
@@ -3,7 +3,7 @@ with cte as ( select * from {{ source('raw', 'olist_sellers_dataset') }} )
 
 SELECT
     seller_id,
-    customer_zip_code_prefix first_five_zip_seller,
+    seller_zip_code_prefix first_five_zip_seller,
     unaccent(seller_city) seller_city,
     seller_state
 FROM 
