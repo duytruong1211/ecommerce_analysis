850ebaf91f96190a86b672773b73a37dde51b57d
duytruong1211 <duytruong1211@gmail.com>
2025-03-29 15:30:57 +0700

    change column name in staging

--- ecommerce_analysis/ecommerce_dwh/models/staging/staging_orders.sql
+++ ecommerce_analysis/ecommerce_dwh/models/staging/staging_orders.sql
@@ -3,6 +3,7 @@ with cte as ( select * from {{ source('raw', 'olist_orders_dataset') }} )
 
 SELECT
     order_id,
+    customer_id user_order_id,
     order_status,    
     COALESCE(NULLIF(order_purchase_timestamp, ''), NULL)::timestamp order_purchase_at,
     COALESCE(NULLIF(order_approved_at, ''), NULL)::timestamp order_approved_at,
